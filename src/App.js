83c00ea9423b5afb80ad09a834bfcfb86850d27e
Mike Wong <wongmdesign@gmail.com>
2020-01-02 17:09:39 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
+function App () {
+  const [data, setData] = useState({ hits: [] })
+  const [query, setQuery] = useState('redux')
+  const [url, setUrl] = useState(
+    'https://hn.algolia.com/api/v1/search?query=redux'
+  )
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false)
+      setIsLoading(true)
+      try {
+        const response = await fetch(url)
+        const json = await response.json()
+
+        setData(json)
+      } catch (error) {
+        setIsError(true)
+      }
+      setIsLoading(false)
+    }
+    fetchData()
+  }, [url])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <form
+        onSubmit={e => {
+          setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`)
+          e.preventDefault()
+        }}
+      >
+        <input
+          type='text'
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type='submit'>Search</button>
+        {isError && <div>Something went wrong..</div>}
+        {isLoading ? (
+          <div>Loading ...</div>
+        ) : (
+          <ul>
+            {data.hits.map(item => (
+              <li key={item.objectID}>
+                <a href={item.url}>{item.title}</a>
+              </li>
+            ))}
+          </ul>
+        )}
+      </form>
+    </>
+  )
 }
-
-export default App;
+export default App
